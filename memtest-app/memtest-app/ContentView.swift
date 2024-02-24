80a543d557f66310952782f7483928f835e6dffa
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-02-24 10:53:02 +0100

    Update ContentView.swift

---
+++ memtest-app/memtest-app/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  memtest-app
+//
+//  Created by Christopher Witzl on 22.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        QRCodeView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
