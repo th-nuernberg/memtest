6fe40643c81b88a69afeadc527597b07beb13c13
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-03-07 15:04:03 +0100

    feat: implement REST-Endpoints through the openapi definition

---
+++ memtest-backend/api/test_result.py
@@ -0,0 +1,4 @@
+def upload_test_result(body):
+    # Implementieren Sie Ihre Logik zum Verarbeiten des TestResult hier
+    print(body)
+    return {"message": "Test result uploaded successfully"}, 200
