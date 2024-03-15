f0e6d5456fe65104e47fa8e610fdcfc8228d6cd6
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-03-15 11:09:03 +0100

    feat: add base class for Tests

---
+++ memtest-app/memtest-app/Views/Tests/BaseTestView.swift
@@ -0,0 +1,18 @@
+//
+//  BaseTestView.swift
+//  memtest-app
+//
+//  Created by Christopher Witzl on 15.03.24.
+//
+
+import SwiftUI
+
+struct BaseTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BaseTestView()
+}
