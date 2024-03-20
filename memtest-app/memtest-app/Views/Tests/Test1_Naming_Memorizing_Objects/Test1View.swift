e065cbec0b386e8d8f356a6836148c1fcf0fb8e3
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-03-20 22:56:46 +0100

    feat: implement basic speech recognition

--- memtest-app/memtest-app/Views/Tests/Test1_Naming_Memorizing_Objects/Test1View.swift
+++ memtest-app/memtest-app/Views/Tests/Test1_Naming_Memorizing_Objects/Test1View.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct Test1View: View {
+    @ObservedObject private var manager = SpeechRecognitionManager()
+    @State private var isRecording = false
+    
     var body: some View {
         BaseTestView(destination: Test2View(), content: {
-            Text("Das ist die Test1View")
+            
+            VStack(spacing: 20) {
+                Text(manager.recognizedText.isEmpty ? "Recognition text will appear here" : manager.recognizedText)
+                                .padding()
+                                .border(Color.gray, width: 1)
+                
+                Button(action: {
+                    self.startStopRecording()
+                }) {
+                    Text(isRecording ? "Stop Recording" : "Start Recording")
+                }
+            }
+            .padding()
+            
         }, explanationContent: {
             Text("Hier sind einige Erklärungen.")
         })
     }
+    
+    private func startStopRecording() {
+           if isRecording {
+               SpeechRecognitionService.shared.stopRecording()
+           } else {
+               do {
+                   try SpeechRecognitionService.shared.startRecording()
+               } catch {
+                   print("Failed to start recording: \(error)")
+               }
+           }
+           isRecording.toggle()
+       }
 }
+
+
+
 
 
 #Preview {
