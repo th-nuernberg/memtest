9610b98b4d72784f4f7709c3ebb882f8a8cd6f65
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-03-30 21:23:14 +0100

    refactor(ContentView): use Welcome View as entrypoint

--- memtest-app/memtest-app/ContentView.swift
+++ memtest-app/memtest-app/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        DragDropCalibrationView()
+        NavigationStack {
+           WelcomeView()
+        }
     }
 }
 
