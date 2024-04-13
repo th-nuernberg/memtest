6284140e1de655c3fce89abfd1754c96d4cd77a9
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-04-13 12:04:42 +0200

    feat(SymbolView): add capability to track taps on symbols and export symbol Map got parent view

--- memtest-app/memtest-app/Views/Tests/Test5/SymbolView.swift
+++ memtest-app/memtest-app/Views/Tests/Test5/SymbolView.swift
@@ -7,66 +7,57 @@
 
 import SwiftUI
 
-struct SymbolPosition {
+struct SymbolPosition: Identifiable {
+    let id: Int
     let symbol: String
-    let position: CGPoint
 }
 
+
 struct SymbolView: View {
-    private let symbols = ["★", "✻", "▢"]
-    private let symbolSize: CGFloat = 30.0
-    
     @StateObject var viewModel: SymbolViewModel
-    @State private var symbolPositions: [SymbolPosition] = []
-    
+
     var body: some View {
         GeometryReader { geometry in
             let rect = CGRect(x: 0, y: 0, width: geometry.size.width, height: geometry.size.height)
             ZStack {
-                ForEach(0..<symbolPositions.count, id: \.self) { index in
-                    let symbolPosition = symbolPositions[index]
-                    Text(symbolPosition.symbol)
-                        .font(.system(size: symbolSize))
-                        .position(symbolPosition.position)
+                ForEach(Array(zip(viewModel.symbolField.indices, viewModel.symbolField)), id: \.0) { index, symbol in
+                    Text(symbol)
+                        .font(.system(size: 30.0))
+                        .position(self.positionForSymbol(index: index, rect: rect, columns: 17, rows: 7))
+                        .onTapGesture {
+                            viewModel.registerTap(on: index, symbolType: symbol)
+                        }
                 }
             }
             .onAppear {
-                initializeView(rect: rect)
+                viewModel.generateSymbolField(numberOfSymbols: 119, symbols: ["★", "✻", "▢"])
             }
         }
     }
-    
-    private func initializeView(rect: CGRect) {
-        viewModel.initializeSymbolCounts(numberOfSymbols: viewModel.numberOfSymbols)
-        generateSymbolsPositions(in: rect)
-        viewModel.selectedSymbol = symbols.randomElement()
-    }
-    
-    private func generateSymbolsPositions(in rect: CGRect) {
-        let maxColumns = 17
-        let rowHeight = rect.height / CGFloat(7)
-        var positions: [SymbolPosition] = []
-        
-        for row in 0..<7 {
-            let columnsInRow = row == 0 ? 16 : 17
-            let columnWidth = rect.width / CGFloat(columnsInRow)
-            
-            for column in 0..<columnsInRow {
-                let x = CGFloat(column) * columnWidth + columnWidth / 2
-                let y = CGFloat(row) * rowHeight + rowHeight / 2
-                let symbol = symbols.randomElement()!
-                positions.append(SymbolPosition(symbol: symbol, position: CGPoint(x: x, y: y)))
-            }
-        }
-        symbolPositions = positions
+
+    private func positionForSymbol(index: Int, rect: CGRect, columns: Int, rows: Int) -> CGPoint {
+        let columnWidth = rect.width / CGFloat(columns)
+        let rowHeight = rect.height / CGFloat(rows)
+        let x = CGFloat(index % columns) * columnWidth + columnWidth / 2
+        let y = CGFloat(index / columns) * rowHeight + rowHeight / 2
+        return CGPoint(x: x, y: y)
     }
 }
+
+
+
+
 
 class SymbolViewModel: ObservableObject {
     @Published var symbolCounts: [String: Int] = ["★": 0, "✻": 0, "▢": 0]
     @Published var selectedSymbol: String?
-    let numberOfSymbols = 117
-    
+    @Published var symbolField: [String] = []
+    @Published var taps: [(Int, String)] = []
+
+    func registerTap(on symbolId: Int, symbolType: String) {
+        taps.append((symbolId, symbolType))
+    }
+
     func initializeSymbolCounts(numberOfSymbols: Int) {
         let symbols = ["★", "✻", "▢"]
         var total = numberOfSymbols
@@ -80,14 +71,13 @@ class SymbolViewModel: ObservableObject {
             symbolCounts[firstSymbol, default: 0] += total
         }
     }
-    
-    var selectedSymbolCount: Int {
-        if let selectedSymbol = selectedSymbol {
-            return symbolCounts[selectedSymbol] ?? 0
-        }
-        return 0
+
+    func generateSymbolField(numberOfSymbols: Int, symbols: [String]) {
+        symbolField = (0..<numberOfSymbols).map { _ in symbols.randomElement()! }
     }
 }
+
+
 
 
 #Preview {
