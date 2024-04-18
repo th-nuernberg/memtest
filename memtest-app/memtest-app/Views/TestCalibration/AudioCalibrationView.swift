e0a8755a04a9523a7932719a8cfa53f1081653a6
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-04-18 10:52:27 +0200

    feat(AudioCalibrationView): add "Nordwind und Sonne" text to calibration View

--- memtest-app/memtest-app/Views/TestCalibration/AudioCalibrationView.swift
+++ memtest-app/memtest-app/Views/TestCalibration/AudioCalibrationView.swift
@@ -18,22 +18,36 @@ struct AudioCalibrationView: View {
                 
                 Spacer()
                 
-                VStack {
-                    Text("Während des Tests wird Ihre Stimme")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(Color(hex: "#5377A1"))
-                    Text("aufgenommen. Oben in der Mitte sehen Sie die ")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(Color(hex: "#5377A1"))
-                    Text("Lautstärke in Grün. Sprechen Sie bitte stets laut")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(Color(hex: "#5377A1"))
-                    Text("und deutlich. Wenn Sie zu leise sprechen,")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(Color(hex: "#5377A1"))
-                    Text("bewegt sich der Lautsprecherpegel nicht.")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(Color(hex: "#5377A1"))
+                if !testStarted {
+                    VStack {
+                        Text("Während des Tests wird Ihre Stimme")
+                            .font(.custom("SFProText-SemiBold", size: 40))
+                            .foregroundStyle(Color(hex: "#5377A1"))
+                        Text("aufgenommen. Oben in der Mitte sehen Sie die ")
+                            .font(.custom("SFProText-SemiBold", size: 40))
+                            .foregroundStyle(Color(hex: "#5377A1"))
+                        Text("Lautstärke in Grün. Sprechen Sie bitte stets laut")
+                            .font(.custom("SFProText-SemiBold", size: 40))
+                            .foregroundStyle(Color(hex: "#5377A1"))
+                        Text("und deutlich. Wenn Sie zu leise sprechen,")
+                            .font(.custom("SFProText-SemiBold", size: 40))
+                            .foregroundStyle(Color(hex: "#5377A1"))
+                        Text("bewegt sich der Lautsprecherpegel nicht.")
+                            .font(.custom("SFProText-SemiBold", size: 40))
+                            .foregroundStyle(Color(hex: "#5377A1"))
+                    }
+                } else {
+                    VStack {
+                        Text("Nordwind und Sonne")
+                            .font(.custom("SFProText-SemiBold", size: 60))
+                            .foregroundStyle(Color(hex: "#5377A1"))
+                            .padding(.bottom)
+                        
+                        Text("Einst stritten sich Nordwind und Sonne, wer von ihnen beiden wohl der Stärkere wäre, als ein Wanderer, der in einen warmen Mantel gehüllt war, des Weges daherkam. Sie wurden einig, dass derjenige für den Stärkeren gelten sollte, der den Wanderer zwingen würde, seinen Mantel abzunehmen. Der Nordwind blies mit aller Macht, aber je mehr er blies, desto fester hüllte sich der Wanderer in seinen Mantel ein. Endlich gab der Nordwind den Kampf auf. Nun erwärmte die Sonne die Luft mit ihren freundlichen Strahlen, und schon nach wenigen Augenblicken zog der Wanderer seinen Mantel aus. Da musste der Nordwind zugeben, dass die Sonne von ihnen beiden der Stärkere war.")
+                            .font(.custom("SFProText-SemiBold", size: 35))
+                            .foregroundStyle(Color(hex: "#5377A1"))
+                    }
+                    .padding(.all)
                 }
                 
                 Spacer()
