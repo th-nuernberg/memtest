e0bff54e454f974b544c6a470bdd3dd4c9046340
Christopher Witzl <43088334+ChipCracker@users.noreply.github.com>
2024-04-24 12:12:46 +0200

    refactor(TestView): Test 6 is now 8, tes 7 is 6, test 8 is 7

--- memtest-app/memtest-app/Views/Tests/Test7/Test7View.swift
+++ memtest-app/memtest-app/Views/Tests/Test7/Test7View.swift
@@ -8,69 +8,155 @@
 import SwiftUI
 
 struct Test7View: View {
+    @ObservedObject private var manager = SpeechRecognitionManager.shared
+    @State private var isRecording = false
     @State private var finished = false
+    @State private var symbols: [TestSymbol]
+    @State private var showingFirstSet = true
+    private let testDuration = 60
+
+    
+    private var symbolList = TestSymbolList()
+    
+    init() {
+       _symbols = State(initialValue: Test7View.initializeSymbols())
+    }
+    
     
     var body: some View {
-        
-        BaseTestView(showCompletedView: $finished, indexOfCircle: 7, textOfCircle: "7", destination: { Test8View()}, content: {
-            VStack {
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            let screenHeight = geometry.size.height - 140
+            let columns = columns()
+            let symbolSize = self.dynamicSymbolSize(forWidth: screenWidth, forHeight: screenHeight, numberOfColumns: columns.count, numberOfSymbols: symbols.count/2)
+            let (firstGroup, secondGroup) = splitSymbolsIntoGroups()
+            
+            BaseTestView(showCompletedView: $finished,indexOfCircle: 7,
+                         textOfCircle:"7", destination: {Test8View()}, content: {
+            
+                //Text(manager.recognizedWords.last ?? "")
                 AudioIndicatorView()
                 Spacer()
-                
                 VStack {
-                    Text("ABBABA")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(.black)
-                        .padding(.bottom, 20)
-                        .underline()
-                    Text("A B A A B A B B A A B A B A B B A")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(.black)
-                    Text("A A B A B A B B B A B A A B A B A")
-                        .font(.custom("SFProText-SemiBold", size: 40))
-                        .foregroundStyle(.black)
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(showingFirstSet ? firstGroup : secondGroup, id: \.name) { symbol in
+                            ZStack {
+                                Rectangle()
+                                  .fill(self.isSymbolNameRecognized(symbol.name) ? Color.gray.opacity(0.5) : .gray)
+                                  .frame(width: symbolSize, height: symbolSize)
+                                  .cornerRadius(20)
+
+
+                                Image(symbol.fileUrl)
+                                  .resizable()
+                                  .aspectRatio(contentMode: .fit)
+                                  .frame(width: symbolSize * 0.75, height: symbolSize * 0.75)
+
+                            }
+                            .padding(.bottom, 10)
+
+                        }
+                    }
+                }
+                .onAppear(perform: {
+                    try! AudioService.shared.startRecording(to: "test8")
+                })
+                .onTimerComplete(duration: testDuration) {
+                    print("Timer completed")
+                    finished = true
+                    AudioService.shared.stopRecording()
                 }
                 
                 Spacer()
                 
-            }
-            .onAppear(perform: {
-                try! AudioService.shared.startRecording(to: "test6");
-            })
-            .onTimerComplete(duration: 60, onComplete: {
-                finished = true
-                AudioService.shared.stopRecording()
-            })
-        }, explanationContent: {
-            HStack {
-                Text("Aufgabenstellung 7")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            
-            VStack{
-                Text("Die siebte Aufgabe besteht darin,")
-                    .font(.custom("SFProText-SemiBold", size: 40))
-                    .foregroundStyle(Color(hex: "#5377A1"))
+                Button(action: {
+                    showingFirstSet.toggle()
+                }) {
+                    Text("Wechseln")
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                }
+                .padding(.horizontal,50)
+                .background(Color.blue)
+                .cornerRadius(10)
                 
-                Text("für jedes A, B zu sprechen und für jedes B, A.")
-                    .font(.custom("SFProText-SemiBold", size: 40))
-                    .foregroundStyle(Color(hex: "#5377A1"))
                 
-                Text("Die Buchstaben sollen nacheinander vorgelesen werden.")
-                    .font(.custom("SFProText-SemiBold", size: 40))
-                    .foregroundStyle(Color(hex: "#5377A1"))
-            }
-            .padding(.top,120)
-        },
-        completedContent: { onContinue in
-            
-            CompletedView( completedTasks: 7, onContinue: onContinue)
-            
-        })
+            }, explanationContent: {
+                HStack {
+                    Text("Aufgabenstellung 7")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                
+                
+                VStack{
+                    Text("Ihre siebte Aufgabe besteht darin, wie")
+                        .font(.custom("SFProText-SemiBold", size: 40))
+                        .foregroundStyle(Color(hex: "#5377A1"))
+                    
+                    Text("bei einem Test davor, die gezeigten Objeke zu erkennen")
+                        .font(.custom("SFProText-SemiBold", size: 40))
+                        .foregroundStyle(Color(hex: "#5377A1"))
+                    
+                    Text("und diese zu bennenen.")
+                        .font(.custom("SFProText-SemiBold", size: 40))
+                        .foregroundStyle(Color(hex: "#5377A1"))
+                    
+                    Text("Sie können zwischen 2 Seiten auf denen Symbole zu sehen sind")
+                        .font(.custom("SFProText-SemiBold", size: 40))
+                        .foregroundStyle(Color(hex: "#5377A1"))
+                        .padding(.top,20)
+                    
+                    Text("wechseln. Benennen Sie die Bilder die Sie sich merken konnten")
+                        .font(.custom("SFProText-SemiBold", size: 40))
+                        .foregroundStyle(Color(hex: "#5377A1"))
+                    
+                }
+                .padding(.top,120)
+            }, completedContent: {onContinue in
+                CompletedView(completedTasks: 7, onContinue: onContinue)
+            })
+        }
+    }
+    
+    private func columns() -> [GridItem] {
+        Array(repeating: .init(.flexible(), spacing: 0), count: 6)
+    }
+
+    private func dynamicSymbolSize(forWidth width: CGFloat, forHeight height: CGFloat, numberOfColumns: Int, numberOfSymbols: Int) -> CGFloat {
+        let totalVerticalSpacing = CGFloat(numberOfSymbols / numberOfColumns) * 10 // Anpassen basierend auf der Anzahl der Zeilen
+        let adjustedHeight = height - totalVerticalSpacing // Verfügbare Höhe nach Abzug des vertikalen Abstands
+        let symbolHeight = adjustedHeight / CGFloat(numberOfSymbols / numberOfColumns) // Höhe jedes Symbols
         
+        let spacing = CGFloat(numberOfColumns - 1) * 10 // Horizontaler Abstand zwischen den Symbolen
+        let adjustedWidth = width - spacing // Verfügbare Breite nach Abzug des horizontalen Abstands
+        let symbolWidth = adjustedWidth / CGFloat(numberOfColumns) // Breite jedes Symbols
+        
+        // Nimmt den kleineren Wert, um sicherzustellen, dass die Symbole nicht außerhalb des sichtbaren Bereichs geraten
+        return min(symbolWidth, symbolHeight)
+    }
+    
+    private func isSymbolNameRecognized(_ name: String) -> Bool {
+        return manager.recognizedWords.contains { $0.lowercased().contains(name.lowercased()) }
+    }
+    
+    // MARK: Static Methods
+    private static func initializeSymbols() -> [TestSymbol] {
+       var symbols = TestSymbolList().symbols
+       for i in 0...35 {
+           symbols.append(TestSymbol(name: "Fragezeichen\(i)", synonyms: [], fileUrl: "Test8Icons/test8_1"))
+       }
+       symbols.shuffle()
+       return symbols
+    }
+    private func splitSymbolsIntoGroups() -> ([TestSymbol], [TestSymbol]) {
+        let midIndex = symbols.count / 2
+        let firstGroup = Array(symbols[..<midIndex])
+        let secondGroup = Array(symbols[midIndex...])
+        return (firstGroup, secondGroup)
     }
 }
 
