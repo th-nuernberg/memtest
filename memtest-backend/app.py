4da34f45fa8c1b2d977baf2bb4f16561c421b4b2
Jonathan Wilisch <wilischjo89084@th-nuernberg.de>
2024-06-20 20:37:34 +0200

    feat: removed flask from the client and refactored

--- memtest-backend/app.py
+++ memtest-backend/app.py
@@ -1,113 +1,119 @@
-import connexion
-from flask import Flask, request, render_template, send_file, jsonify
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import FileStorage
 import os
-import shutil
-from io import BytesIO
-from zipfile import ZipFile
 import tempfile
+import argparse
+import json
+import fitz
 import requests
-#from decrypt import decryptZip  # Ensure this function returns bytes and filename
-from pdfScan import extractQRCodeFromPDF, scanQRCode
+from werkzeug.datastructures import FileStorage
+from zipfile import ZipFile
+from io import BytesIO
+from PIL import Image
+from pyzbar.pyzbar import decode
+import logging
 
-# Erstellen Sie eine Connexion-Anwendung
-app = connexion.FlaskApp(__name__)
+logging.basicConfig(level=logging.INFO, format=' %(levelname)s - %(message)s')
 
-@app.route("/", methods=['GET', 'POST'])
-def upload_file():
+def main():
     
-    if request.method == 'POST':
-        
-        files = []
-        
-        if request.form['fileOption'] == 'single':
-            if 'file' not in request.files:
-                return 'No file part'
+    parser = argparse.ArgumentParser(description="Download and decrypt the patiend data")
+    parser.add_argument('--outdir', type=str, help="Output directory for the patient data")
+    parser.add_argument('--indir', type=str, help='input directory for the patient data (local)')
+    parser.add_argument('--pdfdir', type=str, required=True, help='Directory containing the PDF files')
+    parser.add_argument('pdf_files', nargs='+', type=str, help="PDF files to process (use 'all' to process all PDFs in the directory)")
+    args = parser.parse_args()
+
+    #Check input for validity
+    if(args.indir != None):
+        indir = os.path.normpath(args.indir)
+    else:
+        indir = None
+
+    if(args.outdir != None):
+        outdir = os.path.normpath(args.outdir) 
+    else:
+        outdir = os.path.join(os.path.expanduser("~"), "Downloads")
+
+    if(args.pdfdir != None):
+        pdfdir = os.path.normpath(args.pdfdir)
+    else:
+        pdfdir = os.path.normpath(__file__)
+
+    pdfs = args.pdf_files
+
+    process_files(indir, outdir, pdfdir, pdfs)
+
+def process_files(indir, outdir, pdfdir, pdfs):
+    files = []
+
+    #Check if all the directories exist     
+    if not os.path.exists(pdfdir):
+        logging.error("Invalid pdf input directory")
+        return 
+    if indir != None and not os.path.exists(indir):
+        logging.error("Invalid File Input directory")
+        return
+    if not os.path.exists(outdir):
+        logging.error("Invalid Output directory")
+        return
+    
+    #Pdfs list does not include all => Only certain pdfs should be added to the list
+    if pdfs[0] != 'all':
+        for pdf in pdfs:
+            pdfPath = os.path.join(pdfdir, pdf)
+            if os.path.exists(pdfPath):
+                with open(pdfPath, 'rb') as file:
+                    file_content = file.read()
+                    file_storage = FileStorage(stream=BytesIO(file_content), filename=pdf, name="file", content_type="application/pdf")
+                    files.append(file_storage)
             else:
-                files.append(request.files['file'])
-                
-        elif request.form['fileOption'] == 'multiple':
-            directory = os.path.normpath(request.form["fileDirectory"])
+                logging.error(f"PDF file not found: {pdf}")
+    else:
+        #All pdfs in the pdfdir are added to the list
+        for pdf in os.listdir(pdfdir):
+            if pdf.endswith('.pdf'):
+                pdfPath = os.path.join(pdfdir, pdf)
+                with open(pdfPath, 'rb') as file:
+                    file_content = file.read()
+                    file_storage = FileStorage(stream=BytesIO(file_content), filename=pdf, name="file", content_type="application/pdf")
+                    files.append(file_storage)
+
+    for file in files:
+
+        if file:
+            QRCodes = extractQRCodeFromPDF(file)
             
-            if not os.path.exists(directory):
-                return 'Invalid file directory'
+            if isinstance(QRCodes, str) and ("File does not exist" in QRCodes or "Failed to open PDF document" in QRCodes or "No images found in the PDF document" in QRCodes):
+                logging.error(QRCodes + file.filename) #log the error and continue with the next file
+                continue
             
-            for filename in os.listdir(directory):
-                if filename.endswith(".pdf"):
-                    file_path = os.path.join(directory, filename)
-                    with open(file_path, 'rb') as file:
-                        file_content = file.read()
-                        file_storage = FileStorage(stream=BytesIO(file_content), filename=filename, name='file', content_type='application/pdf')
-                        files.append(file_storage)
-        
-        if request.form['fileLocation'] == 'local':
-            if not os.path.exists(os.path.normpath(request.form["inputDirectory"])):
-                return "Invalid input directory"
-        
-        errors = [] #keep a list of all errors that happen during requests
-        
-        for file in files:
-    
-            filename = secure_filename(os.path.basename(file.name))
+            if os.path.exists(outdir):
+                for code in QRCodes:
+                    QRCodeData = scanQRCode(code)
+                    if isinstance(QRCodeData, str) and ("Couldn't retrieve id and key" in QRCodeData or "Failed to decode QR code" in QRCodeData):
+                        logging.error(QRCodeData + " in " + file.filename) #log the error message and continue
+                        continue
+                    
+                    if indir != None: #Filelocation is local
+                        if os.path.exists(os.path.join(indir, QRCodeData["id"] + ".zip")):
+                            removePasswordAndSave(os.path.join(indir, QRCodeData["id"] + ".zip"), QRCodeData["key"], QRCodeData["id"], outdir)
+                        else:
+                            logging.error(f"Test result not found for {QRCodeData['id']}")
+                            continue
+                    else: #fileLocation == remote
+                        error = getZipFromRemote(QRCodeData["id"], QRCodeData["key"], outdir)
+                        if error:
+                            logging.error(error)
+                            continue
+                                            
+            else:
+                logging.error("Invalid output directory")
+                return
+    return
 
-            if file.filename == '':
-                return 'No selected file'
-
-            if file:
-                filePath = saveFileToStatic(file)
-                QRCodes = extractQRCodeFromPDF(filePath)
-                os.remove(filePath)
-                
-                if isinstance(QRCodes, str) and ("File does not exist" in QRCodes or "Failed to open PDF document" in QRCodes or "No images found in the PDF document" in QRCodes):
-                    errors.append(QRCodes)
-                    continue
-                
-                # Create a temporary directory to store the decrypted ZIP files
-                with tempfile.TemporaryDirectory() as tmpdirname:
-                    outputDirectory = os.path.normpath(request.form["outputDirectory"])
-                    if os.path.exists(outputDirectory):
-                        for code in QRCodes:
-                            QRCodeData = scanQRCode(code)
-                            if isinstance(QRCodeData, str) and ("Couldn't retrieve id and key" in QRCodeData or "Failed to decode QR code" in QRCodeData):
-                                errors.append(QRCodeData)
-                                continue
-                            
-                            inputDir = os.path.normpath(request.form["inputDirectory"])
-                            
-                            if request.form["fileLocation"] == "local":
-                                if os.path.exists(os.path.join(inputDir, QRCodeData["id"] + ".zip")):
-                                    decryptedZip, zipFileName = removePassword(os.path.join(inputDir, QRCodeData["id"] + ".zip"), QRCodeData["key"], QRCodeData["id"])
-                                else:
-                                    errors.append(f"Test result not found for {QRCodeData['id']}")
-                                    continue
-                            else: #fileLocation == remote
-                                decryptedZip, zipFileName, error = getZipFromRemote(QRCodeData["id"], QRCodeData["key"])
-                                if error:
-                                    errors.append(error)
-                                    continue
-                                    
-                            # Save the decrypted ZIP file in the temporary directory
-                            decryptedZipPath = os.path.join(tmpdirname, zipFileName)
-                            with open(decryptedZipPath, 'wb') as f:
-                                shutil.copyfileobj(decryptedZip, f)
-                                                    
-                        for filename in os.listdir(tmpdirname):
-                            filePath = os.path.join(tmpdirname, filename)
-                            saveFileToOutput(filePath, outputDirectory, filename)
-                    else:
-                        return 'Invalid output directory'
-        allErrors = errors
-        errors = []
-        return render_template('success.html', errors=allErrors)
-    return render_template('upload.html')
-
-def getZipFromRemote(id, key):
+def getZipFromRemote(id, key, outdir):
     url = f"http://127.0.0.1:8080/api/v1/testresult/{id}"
     
     error = None
-    decryptedZip = None
-    zipFileName = None
     
     try:
         response = requests.get(url)
@@ -116,7 +122,7 @@ def getZipFromRemote(id, key):
             with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
                 tmpfile.write(response.content)
                 path = tmpfile.name
-            decryptedZip, zipFileName = removePassword(path, key, id)
+            removePasswordAndSave(path, key, id, outdir)
         elif response.status_code == 404:
             error = f"Test result not found for {id}"
         elif response.status_code == 500:
@@ -124,34 +130,68 @@ def getZipFromRemote(id, key):
         else:
             error = f"Unexpected error occurred for {id} error code: {response.status_code}"
     except requests.RequestException as e:
-        print(f"Request failed: {e}")
-    return decryptedZip, zipFileName, error
+        logging.error(f"Request failed: {e}")
+    return error
 
-def saveFileToStatic(file):
-    filename = secure_filename(file.filename)
-    splitFilename = filename.rsplit('.', 1)
-    if len(splitFilename) == 2 and splitFilename[1]:
-        filePath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static", "files", filename) 
-        file.save(filePath)
-        return filePath
-
-def saveFileToOutput(src_file, outputdir, filename):
-    output_path = os.path.join(outputdir, filename)
-    shutil.copyfile(src_file, output_path)
-
-def removePassword(zipFilePath, key, id):
-    zipName = f"{id}.zip"
-
-    decryptedZip = BytesIO()
+def removePasswordAndSave(zipFilePath, key, id, outdir):
 
     with ZipFile(zipFilePath, 'r') as zip_ref:
-        zip_ref.extractall(pwd=bytes(key, 'utf-8'))
-        with ZipFile(decryptedZip, 'w') as new_zip:
-            for file_name in zip_ref.namelist():
-                new_zip.write(file_name)
+        zip_ref.extractall(pwd=bytes(key, 'utf-8'), path=outdir)
+    logging.info(f"Successfully downloaded the test data with the id: {id} into the output directory")
+    return
+
+#PDFscan functions
+
+def extractQRCodeFromPDF(fileStorage):
     
-    decryptedZip.seek(0)
-    return decryptedZip, zipName
+    fileContent = fileStorage.read()
+    fileBytes = BytesIO(fileContent)
+
+    document = fitz.open(stream = fileBytes, filetype="pdf")
+    
+    if not document:
+        return f'Failed to open {fileStorage.filename}'
+    
+    pageNums = len(document)
+    images = []
+    qrCodes = []
+    
+    for pageNum in range(pageNums):
+        pageContent = document[pageNum]
+        images.extend(pageContent.get_images())
+        
+    if len(images) == 0:
+        return "No images found in the PDF document"
+        
+    for i, image in enumerate(images, start=1):
+        xref = image[0]
+        baseImage = document.extract_image(xref)
+        imageBytes = baseImage['image']
+        
+        pilImage = Image.open(BytesIO(imageBytes))
+        
+        decodedImage = decode(pilImage)
+        
+        if decodedImage:
+            qrCodes.append(pilImage)
+    
+    return qrCodes
+
+def scanQRCode(QRCode):
+    decoded = decode(QRCode)
+    
+    if not decoded:
+        return 'Failed to decode QR code'
+    
+    try:
+        data = json.loads(decoded[0].data)
+    except json.JSONDecodeError:
+        return 'Couldn\'t retrieve id and key from the QR code'
+    
+    if "id" not in data or "key" not in data:
+        return 'Couldn\'t retrieve id and key from the QR code'
+    
+    return data
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=3000)
+    main()
