a6115b4dcddc5abf364cc70b87314687e3c18561
Jonathan Wilisch <wilischjo89084@th-nuernberg.de>
2024-06-24 22:44:09 +0200

    chore: added additional comments for clarity

--- memtest-tools/download.py
+++ memtest-tools/download.py
@@ -15,9 +15,10 @@ logging.basicConfig(level=logging.INFO, format=' %(levelname)s - %(message)s')
 
 def main():
     
+    #Set up argument parser
     parser = argparse.ArgumentParser(description="Download and decrypt the patiend data")
     parser.add_argument('--outdir', type=str, help="Output directory for the patient data (If left empty uses the downloads folder as output)")
-    parser.add_argument('--indir', type=str, help='input directory for the patient data (local)')
+    parser.add_argument('--indir', type=str, help='input directory for the patient data (If left empty fetches them from the server)')
     parser.add_argument('--pdfdir', type=str, help='Directory containing the PDF files')
     parser.add_argument('pdf_files', nargs='+', type=str, help="PDF files to process (use 'all' to process all PDFs in the directory)")
     args = parser.parse_args()
@@ -91,19 +92,19 @@ def process_files(indir, outdir, pdfdir, pdfs):
                 for code in QRCodes:
                     QRCodeData = scanQRCode(code)
                     if isinstance(QRCodeData, str) and ("Couldn't retrieve id and key" in QRCodeData or "Failed to decode QR code" in QRCodeData):
-                        logging.error(QRCodeData + " in " + file.filename) #log the error message and continue
+                        logging.error(QRCodeData + " in " + file.filename) #log the error and continue with the next file
                         continue
                     
                     if indir != None: #Filelocation is local
                         if os.path.exists(os.path.join(indir, QRCodeData["id"] + ".zip")):
-                            removePasswordAndSave(os.path.join(indir, QRCodeData["id"] + ".zip"), QRCodeData["key"], QRCodeData["id"], outdir)
+                            removePasswordAndSave(os.path.join(indir, QRCodeData["id"] + ".zip"), QRCodeData["key"], outdir)
                         else:
-                            logging.error(f"Test result not found for {QRCodeData['id']}")
+                            logging.error(f"Test result not found for {QRCodeData['id']}") #Lof the error and continue with the next file
                             continue
-                    else: #fileLocation == remote
+                    else: #fileLocation is remote
                         error = getZipFromRemote(QRCodeData["id"], QRCodeData["key"], outdir)
                         if error:
-                            logging.error(error)
+                            logging.error(error) #Log all the errors that happened during the fetching from remote
                             continue
                                             
             else:
@@ -112,9 +113,9 @@ def process_files(indir, outdir, pdfdir, pdfs):
     return
 
 def getZipFromRemote(id, key, outdir):
-    url = f"http://127.0.0.1:8080/api/v1/testresult/{id}"
+    url = f"http://127.0.0.1:8080/api/v1/testresult/{id}" #Change this url to the server url
     
-    error = None
+    error = None #Variable to store errors in, so that it can be logged later
     
     try:
         response = requests.get(url)
@@ -123,7 +124,8 @@ def getZipFromRemote(id, key, outdir):
             with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
                 tmpfile.write(response.content)
                 path = tmpfile.name
-            removePasswordAndSave(path, key, id, outdir)
+            removePasswordAndSave(path, key, outdir)
+        #If an error happens, set the errors variable to it so that it can be logged later
         elif response.status_code == 404:
             error = f"Test result not found for {id}"
         elif response.status_code == 500:
@@ -134,8 +136,8 @@ def getZipFromRemote(id, key, outdir):
         logging.error(f"Request failed: {e}")
     return error
 
-def removePasswordAndSave(zipFilePath, key, id, outdir):
-
+def removePasswordAndSave(zipFilePath, key, outdir):
+    #Remove the password from the zipfile using the key from the qrcode and extract it into the output directory
     with ZipFile(zipFilePath, 'r') as zip_ref:
         zip_ref.extractall(pwd=bytes(key, 'utf-8'), path=outdir)
     return
@@ -150,7 +152,7 @@ def extractQRCodeFromPDF(fileStorage):
     document = fitz.open(stream = fileBytes, filetype="pdf")
     
     if not document:
-        return f'Failed to open {fileStorage.filename}'
+        return f'Failed to open {fileStorage.filename}' #Error is logged later
     
     pageNums = len(document)
     images = []
@@ -161,35 +163,35 @@ def extractQRCodeFromPDF(fileStorage):
         images.extend(pageContent.get_images())
         
     if len(images) == 0:
-        return "No images found in the PDF document"
+        return "No images found in the PDF document"#Error is logged later
         
     for i, image in enumerate(images, start=1):
         xref = image[0]
-        baseImage = document.extract_image(xref)
+        baseImage = document.extract_image(xref) #Extract every image from the pdf
         imageBytes = baseImage['image']
         
         pilImage = Image.open(BytesIO(imageBytes))
         
-        decodedImage = decode(pilImage)
+        decodedImage = decode(pilImage) #Attempt to decode the image
         
         if decodedImage:
-            qrCodes.append(pilImage)
+            qrCodes.append(pilImage)#If the decoded image returns data it is a qrcode and is added to the list
     
     return qrCodes
 
 def scanQRCode(QRCode):
-    decoded = decode(QRCode)
+    decoded = decode(QRCode)#Decode the qrcode
     
     if not decoded:
-        return 'Failed to decode QR code'
+        return 'Failed to decode QR code'#If reading qrcode fails return this error to be logged later
     
     try:
         data = json.loads(decoded[0].data)
     except json.JSONDecodeError:
-        return 'Couldn\'t retrieve id and key from the QR code'
+        return 'Couldn\'t retrieve id and key from the QR code'#If the qrcode has no data in the position the key and the id would be return this error to be logged later
     
     if "id" not in data or "key" not in data:
-        return 'Couldn\'t retrieve id and key from the QR code'
+        return 'Couldn\'t retrieve id and key from the QR code'#If the qrcode has no key or id attribute return the error to be logged later
     
     return data
 
