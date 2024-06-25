c957a837f480667417c2fef281de83ae21791d62
werzingerma <werzingerma89360@th-nuernberg.de>
2024-06-25 16:38:32 +0200

    feat(backend): add studykey confirmation

--- memtest-backend-for-upload/openapi_server/__main__.py
+++ memtest-backend-for-upload/openapi_server/__main__.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+from pathlib import Path
+
+# Set the current working directory to the parent directory
+current_dir = Path(__file__).resolve().parent
+project_root = current_dir.parent
+sys.path.insert(0, str(project_root))
+
 import connexion
-
 from openapi_server import encoder
-
 
 def main():
     app = connexion.App(__name__, specification_dir='../../memtest-app/memtest-server-client/')
@@ -13,7 +20,6 @@ def main():
                 pythonic_params=True)
 
     app.run(port=8080)
-
 
 if __name__ == '__main__':
     main()
